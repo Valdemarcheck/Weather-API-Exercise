e5c51b48af218a65762dc12109da735d80ed1a2d
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-09-02 19:25:56 +0500

    Setup basic file structure

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  plugins: [new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  devtool: "eval",
+  devServer: {
+    static: "./dist",
+  },
+};
