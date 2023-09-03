3d82cb62e15549d8d8c29d42cc71b4c283510ae0
Valdemar_check <valekzhaninvladimir58@gmail.com>
2023-09-03 13:29:08 +0500

    Setup local storage management

--- src/app.js
+++ src/app.js
@@ -1,44 +1,55 @@
 import TextTranslator from "./textTranslator";
+import LocalStorageManager from "./localStorageManager";
 
 export default class App {
+  TEXT_TRANSLATIONS = {
+    "form#settings>.header": {
+      ru: "Настройки",
+      en: "Settings",
+    },
+    'label[for="language"]': {
+      ru: "Язык",
+      en: "Language",
+    },
+    'label[for="speed"]': {
+      ru: "Скорость",
+      en: "Speed",
+    },
+    'label[for="temperature"]': {
+      ru: "Температура",
+      en: "Temperature",
+    },
+    'label[for="pressure"]': {
+      ru: "Давление",
+      en: "Pressure",
+    },
+    'label[for="precipitation"]': {
+      ru: "Осадки",
+      en: "Precipitation",
+    },
+    'label[for="location"]': {
+      ru: "Город или страна",
+      en: "City or country",
+    },
+  };
+
   constructor() {}
 
   setup() {
-    const languageSelect = document.getElementById("language");
-    const TEXT_TRANSLATIONS = {
-      "form#settings>.header": {
-        ru: "Настройки",
-        en: "Settings",
-      },
-      'label[for="language"]': {
-        ru: "Язык",
-        en: "Language",
-      },
-      'label[for="speed"]': {
-        ru: "Скорость",
-        en: "Speed",
-      },
-      'label[for="temperature"]': {
-        ru: "Температура",
-        en: "Temperature",
-      },
-      'label[for="pressure"]': {
-        ru: "Давление",
-        en: "Pressure",
-      },
-      'label[for="precipitation"]': {
-        ru: "Осадки",
-        en: "Precipitation",
-      },
-      'label[for="location"]': {
-        ru: "Город или страна",
-        en: "City or country",
-      },
-    };
+    const localStorageManager = new LocalStorageManager();
+    const textTranslator = new TextTranslator(this.TEXT_TRANSLATIONS);
 
-    const textTranslator = new TextTranslator(TEXT_TRANSLATIONS);
+    const languageSelect = document.getElementById("language");
+
+    if (localStorageManager.getValue("language")) {
+      const language = localStorageManager.getValue("language");
+      textTranslator.translateText(language);
+      languageSelect.value = language;
+    }
+
     languageSelect.addEventListener("change", () => {
       textTranslator.translateText(languageSelect.value);
+      localStorageManager.setValue("language", languageSelect.value);
     });
   }
 }
